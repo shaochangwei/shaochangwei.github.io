ca7af022979782aa47489b1999ab254d89b7b844
邵常伟 <shaochangwei@Mojave.local>
2019-09-19 22:38:04 +0800

    add

---
+++ precache-manifest.21182057291a729ba7da011f2d97eb8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92100f106442823dd07513216dc91ec0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49e4ccda267a40904819",
+    "url": "/static/css/main.a2b78b48.chunk.css"
+  },
+  {
+    "revision": "d6bf1b54c888c695e9c2",
+    "url": "/static/js/2.e910a405.chunk.js"
+  },
+  {
+    "revision": "49e4ccda267a40904819",
+    "url": "/static/js/main.21ef51ad.chunk.js"
+  },
+  {
+    "revision": "650c2797114493cf370a",
+    "url": "/static/js/runtime~main.64229f89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
